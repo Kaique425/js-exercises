8a9d30a20027f647682a33360d6fe2c50bd5d088
Alexander Lockshyn <info@lab37.com>
2022-06-27 15:48:23 -0300

    teste git resolve

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
-for(var i = 0; i < 6; i++){
-    console.log(`O valor de i é ${i}`)
+
+const juntaDoisArrays = () => {
+    const first_array = ["http//image.com"]
+    const second_array = [{posts:"teste"}];
+
+    const farrayAndsarray = second_array.map((post, index) => {
+        return console.log({...post ,cover: first_array[index]})
+    })
 }
+
+console.log(juntaDoisArrays())
